a7789bbb204cb45d4b2a2826786842f34e8a8d60
Diego Monroy <difemo91@gmail.com>
2020-05-28 03:21:27 +0000

    [FEAT] add initial files is palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is palindrome
+ * @head: double pointer to the head of list
+ * Return: 1 if it is palindrome, 0 if it is not
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL)
+		return (1);
+	return (1);
+}
