682b0450db4677b1417cd316a0cde86c1a365d80
Diego Monroy <difemo91@gmail.com>
2020-06-02 00:15:57 +0000

    [FEAT] add log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,5 +1,40 @@
 #!/usr/bin/python3
+"""
+    Computes stdin input
+"""
+
+
 import sys
 
+
+def addFileSize(data):
+    return int(data.split()[8])
+
+
+def codes(data):
+    return data.split()[7]
+
+counter = 0
+total_size = 0
+code_status = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
 for line in sys.stdin:
-    print(line)
+    counter += 1
+    total_size += addFileSize(line)
+    code = codes(line)
+    if code in code_status:
+        code_status[code] += 1
+    if counter == 10:
+        print("File size: {:d}".format(total_size))
+        for code, value in sorted(code_status.items()):
+            print("{:s}: {:d}".format(code, value))
+        counter = 0
