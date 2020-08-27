0b4ea88ea8ad8b2a1ca9f374bd17e6b2ae78325a
Diego Monroy <difemo91@gmail.com>
2020-08-26 19:48:34 -0500

    [FEAT] add rain method structure

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Rain
+"""
+
+
+def rain(walls: int) -> int:
+    """calculate how much water will be
+       retained after it rains
+    """
+    if walls:
+        return walls
+    return 0
