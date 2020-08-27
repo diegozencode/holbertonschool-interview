349c9eb92c0ddd8ac46912ec0671e410e1df624c
Diego Monroy <difemo91@gmail.com>
2020-08-26 21:18:27 -0500

    [FEAT] add rain algorithm

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -4,10 +4,23 @@ Rain
 """
 
 
-def rain(walls: int) -> int:
+def rain(walls):
     """calculate how much water will be
        retained after it rains
     """
-    if walls:
-        return walls
-    return 0
+    if len(walls) == 0:
+        return 0
+
+    max_water = 0
+
+    for n in range(len(walls)):
+        max_left = walls[n]
+        for num in range(n):
+            max_left = max(max_left, walls[num])
+        max_right = walls[n]
+        for num in range(n + 1, len(walls)):
+            max_right = max(max_right, walls[num])
+
+        max_water = max_water + (min(max_left, max_right) - walls[n])
+
+    return max_water
