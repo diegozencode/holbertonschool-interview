be9c70844b628038461e6ae0886cad58d78e72e4
Diego Monroy <difemo91@gmail.com>
2020-09-09 14:52:17 -0500

    [FEAT] add advanced binary search

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -8,9 +8,44 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-	{
-		return (-1);
-	}
+	int left = 0;
+	int right = 0;
+	int index = 0;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
+	right = (int)size;
+	index = binary_search(array, left, right, value);
+	return (index);
+}
+
+/**
+ * binary_search - recursive search
+ * @array: pointer to the first element
+ * @left: first element
+ * @right: last element
+ * @value: the value to search for
+ * Return: index
+ */
+int binary_search(int *array, int left, int right, int value)
+{
+	printf("Searching in array: ");
+	for (int i = left; i < right; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+
+	if (left > right)
+		return (-1);
+
+	int mid = (left + right) / 2;
+
+	if (value == array[mid])
+		return (mid);
+	else if (value < array[mid])
+		return (binary_search(array, left, mid - 1, value));
+	else
+		return (binary_search(array, mid + 1, right, value));
 }
--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -5,5 +5,6 @@
 #include <stdlib.h>
 
 int advanced_binary(int *array, size_t size, int value);
+int binary_search(int *array, int left, int right, int value);
 
 #endif /* SEARCH_ALGOS_H */
