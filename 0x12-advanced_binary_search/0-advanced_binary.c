eff6a2c4b9442ef64d6730f759c8038c4926472b
Diego Monroy <difemo91@gmail.com>
2020-09-09 13:17:31 -0500

    [FEAT] add initial files advanced binary search

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+/**
+ * advanced_binary - searches a value in an array of integers
+ * @array: pointer to the first element
+ * @size: number of elements in array
+ * @value: the value to search for
+ * Return: index where value is located
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	return (value);
+}
