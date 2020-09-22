ce45b4bdc581885af22cc8d88afb3851e45156fc
Diego Monroy <difemo91@gmail.com>
2020-09-22 17:51:37 -0500

    [FEAT] add heap - extract initial files

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "binary_trees.h"
+/**
+ * heap_extract - extracts the root node of a max binary heap
+ * @root: a double pointer to the root node of the heap
+ * Return: the value stored in root or 0 if fails
+ */
+int heap_extract(heap_t **root)
+{
+	if (!root)
+		return (0);
+	return (1);
+}
