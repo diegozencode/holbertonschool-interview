f169c490917b6acad2ec83218e5ae4755217f584
Diego Monroy <difemo91@gmail.com>
2021-01-14 22:35:45 -0500

    [FEAT - SANDPILES] add validation for zero sandpile

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -6,26 +6,43 @@
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	print_grid(grid1);
-	print_grid(grid2);
+	int valid_grid1;
+	int i, j;
+
+	valid_grid1 = sandpiles_test(grid1);
+
+	if (valid_grid1 == 0)
+	{
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid1[i][j] = grid2[i][j];
+			}
+		}
+	}
 }
 
 /**
- * print_grid - iterates over the grid and prints it
- * @grid: grid 3x3 of integers
+ * sandpiles_test - validate that all grid is 0
+ * @grid1: grid 3x3 of integers 
+ * Return: validation 0 for true and 1 for false
  */
-static void print_grid(int grid[3][3])
+int sandpiles_test(int grid1[3][3])
 {
+	int valid = 0;
 	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			if (j)
-				printf(" ");
-			printf("%d", grid[i][j]);
+			if (grid1[i][j] != 0)
+			{
+				valid = 1;
+			}
+			break;
 		}
-		printf("\n");
 	}
+	return (valid);
 }
