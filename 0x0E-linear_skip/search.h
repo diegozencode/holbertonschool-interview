f9866874418af27cfb99f50fba55e7e846b34930
Diego <difemo91@gmail.com>
2021-03-31 01:57:51 +0000

    [FEAT - LINEAR SKIP] add prototypes of other function to the header file

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -24,5 +24,9 @@ typedef struct skiplist_s
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
+skiplist_t *linear_skip(skiplist_t *head, int value);
 
 #endif /* SEARCH_H */
