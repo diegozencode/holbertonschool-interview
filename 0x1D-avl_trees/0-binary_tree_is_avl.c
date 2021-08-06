dbb5a35a7534f7137eef2b773b21b7460d0776cd
Diego Monroy <difemo91@gmail.com>
2021-08-06 01:51:24 +0000

    [FEAT - AVL] add function to validate avl trees

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -6,5 +6,38 @@
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	return (1);
+	int height;
+
+	if (!tree)
+		return (0);
+
+	return (is_avl(tree, INT_MIN, INT_MAX, &height));
+}
+
+/**
+ * is_avl_tree - checks if a tree is a valid AVL tree
+ * @tree: pointer to the root node of the tree to check
+ * @min: current minimun value
+ * @max: current maximum value
+ * @height: current height
+ * Return: 1 if the tree is valid AVL, 0 otherwise
+ */
+int is_avl(const binary_tree_t *tree, int min, int max, int *height)
+{
+	int h1 = 0;
+	int h2 = 0;
+
+	if (!tree)
+		return (1);
+
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+
+	if (!is_avl(tree->left, min, tree->n, &h1) ||
+		!is_avl(tree->right, tree->n + 1, max, &h2))
+		return (0);
+
+	*height = MAX(h1, h2) + 1;
+
+	return (ABS(h1 - h2) <= 1);
 }
--- 0x1D-avl_trees/binary_trees.h
+++ 0x1D-avl_trees/binary_trees.h
@@ -1,6 +1,12 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <limits.h>
+#include <stdio.h>
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,15 +17,16 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_avl(const binary_tree_t *tree);
+int is_avl(const binary_tree_t *tree, int min, int max, int *height);
 
 #endif /* BINARY_TREES_H*/
