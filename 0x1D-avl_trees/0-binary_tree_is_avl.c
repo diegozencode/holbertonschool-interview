7e60feb848de0258cfe4b38797ec1c32713cd96c
Diego Monroy <difemo91@gmail.com>
2021-08-06 00:09:42 +0000

    [FEAT - AVL] add project description and initial files

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h";
+/**
+ * binary_tree_is_avl - checks if a tree is a valid AVL tree
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if the tree is valid AVL, 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	return (1);
+}
