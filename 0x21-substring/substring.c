fa814c368c84e80a398f994220f7287f7f87439d
Diego Monroy <difemo91@gmail.com>
2021-08-19 20:24:18 +0000

    [FEAT - SUBSTRING] add function that finds all the possible substrings containing a list of words

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -1,4 +1,5 @@
 #include "substring.h"
+#include <string.h>
 
 /**
  * find_substring - finds a substring in a string
@@ -10,5 +11,44 @@
  **/
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	return (NULL);
+	int i, j, k;
+	int current, count, validate;
+	int len, words_len;
+	int *index, *match;
+
+	if (s == NULL || words == NULL || nb_words < 1 || n == NULL)
+		return (NULL);
+
+	len = strlen(s);
+	words_len = strlen(words[0]);
+	index = malloc(sizeof(int) * len);
+	if (index == NULL)
+		return (NULL);
+	match = malloc(sizeof(int) * nb_words);
+	if (match == NULL)
+		return (NULL);
+	for (i = count = 0; i <= len - nb_words * words_len; i++)
+	{
+		memset(match, 0, sizeof(int) * nb_words);
+		for (j = 0; j < nb_words; j++)
+		{
+			for (k = 0; k < nb_words; k++)
+			{
+				current = i + j * words_len;
+				validate = strncmp(s + current, words[k], words_len);
+				if (!*(match + k) && !validate)
+				{
+					*(match + k) = 1;
+					break;
+				}
+			}
+			if (k == nb_words)
+				break;
+		}
+		if (j == nb_words)
+			*(index + count++) = i;
+	}
+	free(match);
+	*n = count;
+	return (index);
 }
