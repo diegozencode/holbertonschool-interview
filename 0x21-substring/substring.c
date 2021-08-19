beeb68356259a9d74b48503a4b22253c49506f48
Diego Monroy <difemo91@gmail.com>
2021-08-19 19:25:39 +0000

    [DOCS - SUBSTRING] add project description

---
+++ 0x21-substring/substring.c
@@ -0,0 +1,14 @@
+#include "substring.h"
+
+/**
+ * find_substring - finds a substring in a string
+ * @s: string to scan
+ * @words: array of words to search for
+ * @nb_words: number of words to search for
+ * @n: address of the number of words found
+ * Return: allocated array storing each index in s or NULL if not found
+ **/
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	return (NULL);
+}
